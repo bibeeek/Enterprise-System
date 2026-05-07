c40309e18166d312d058fd58d976d82b896199e3
Bibeeek <pokhrel12.bibek@gmail.com>
2026-05-07 15:13:41 +0545

    add cors config

---
+++ demo/src/main/java/com/EnterpriseSystem/demo/Authentication/JwtImpl/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.EnterpriseSystem.demo.Authentication.JwtImpl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
